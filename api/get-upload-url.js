51b3e1b38c416624d44bab40c83af7a1ca9d7689
Othniel Obasi <122861704+othnielObasi@users.noreply.github.com>
2026-02-08 23:27:54 +0000

    Add runtime configuration for get-upload-url

--- api/get-upload-url.js
+++ api/get-upload-url.js
@@ -88,3 +88,6 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: e?.message || "Unknown error" });
   }
 }
+export const config = {
+  runtime: "nodejs",
+};
